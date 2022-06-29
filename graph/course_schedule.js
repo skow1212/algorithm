9342216238ec8a5f70a3ff046008edec4acc42a5
Dohun Yeo <dyeo1992@gmail.com>
2022-06-30 07:16:35 +0900

    seventh

--- graph/course_schedule.js
+++ graph/course_schedule.js
@@ -3,30 +3,79 @@
  * @param {number[][]} prerequisites
  * @return {boolean}
  */
+//dfs
 var canFinish = function (numCourses, prerequisites) {
-  let matrix = new Array(numCourses).fill(new Array(numCourses));
-  let ing = new Array(numCourses);
-  for (let i = 0; i < prerequisites.length; i++) {
-    let ready = prerequisites[i][0];
-    let pre = prerequisites[i][1];
-    ing[ready] = matrix[pre][ready] === 0 ? ing[ready] + 1 : ing[ready];
-    matrix[pre][ready] = 1;
-  }
-  let count = 0;
-  let q = [];
-  ing.forEach((i, course) => {
-    if (course === 0) {
-      q.push(i);
+  const dfs = (crs, visited, map) => {
+    if (visited.has(crs)) {
+      return false;
     }
-  });
-  while (q.length) {
-    let course = q.shift();
-    count++;
-    for (let i = 0; i < numCourses; i++) {
-      if (matrix[course][i] != 0 && --ing[i] === 0) {
-        q.push(i);
+    if (!map.get(crs).length) {
+      return true;
+    }
+    visited.add(crs);
+    for (let pre of map.get(crs)) {
+      if (!dfs(pre, visited, map)) {
+        return false;
       }
     }
+    visited.delete(crs);
+    map.set(crs, []);
+    return true;
+  };
+  let map = new Map();
+  for (let i = 0; i < numCourses; i++) {
+    map.set(i, []);
   }
-  return count === numCourses;
+  prerequisites.forEach((pre) => {
+    map.get(pre[0]).push(pre[1]);
+  });
+  let visited = new Set();
+  for (let crs = 0; crs < numCourses; crs++) {
+    if (!dfs(crs, visited, map)) {
+      return false;
+    }
+  }
+  return true;
 };
+//bfs
+var canFinish = function (numCourses, prerequisites) {
+  const q = [];
+  const map = new Map();
+  const indegree = Array(numCourses).fill(0);
+  for (const [e, v] of prerequisites) {
+    // build map map
+    if (map.has(v)) {
+      map.get(v).push(e);
+    } else {
+      map.set(v, [e]);
+    }
+    // build indegree array
+    indegree[e]++;
+  }
+
+  for (let i = 0; i < indegree.length; i++) {
+    if (indegree[i] === 0) q.push(i);
+  }
+  let count = 0;
+  while (q.length) {
+    const v = q.shift();
+    if (map.has(v)) {
+      for (const e of map.get(v)) {
+        indegree[e]--;
+        if (indegree[e] === 0) q.push(e);
+      }
+    }
+    count++;
+  }
+
+  return numCourses === count;
+};
+let test = [
+  [1, 0],
+  [2, 6],
+  [1, 7],
+  [6, 4],
+  [7, 0],
+  [0, 5],
+];
+console.log(canFinish(8, test));
